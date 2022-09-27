3c6dd927a9c2904e1c4ea31ab21e94100b42a908
leminh4000 <leminh2000@gmail.com>
2022-09-27 19:17:59 +0700

    init

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from "react-navigation";
+
+const SearchScreen = ()=>{
+    return <View>
+        <Text>Search Screen</Text>
+    </View>
+}
+
+export default SearchScreen;
